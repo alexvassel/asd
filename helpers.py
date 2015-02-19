466a459214da4143279b3abc6f6b6530e1739e58
Alexander Vasilyev <av@av.users.rbc.ru>
2015-02-19 17:15:42 +0400

    initial

--- helpers.py
+++ helpers.py
@@ -1,1 +1,60 @@
 # -*- coding: utf-8 -*-
+
+import bz2
+
+# Имя POST параметра
+POST_PARAM_NAME = 'string'
+
+# Доступные действия
+ALLOWED_ACTIONS = ('compress', 'decompress')
+
+
+class Processor(object):
+    """
+    Класс для архивирования, разархивирования строк
+    Принимает в конструкторе объект, который предоставляет методы, характерные для файла
+    """
+
+    # Размер чанка для чтения
+    CHUNK_SIZE = 32
+
+    def __init__(self, file_object):
+        self.buffer = file_object
+
+    def compress(self):
+        """
+        архивирование
+        """
+        compressor = bz2.BZ2Compressor()
+
+        # Читаем и файлоподобный объект чанками
+        while True:
+            block = self.buffer.read(self.CHUNK_SIZE)
+            if not block:
+                break
+            compressor.compress(block)
+
+        # Завершение процесса архивирования
+        response = compressor.flush()
+
+        # Отдаем чанками
+        while response:
+            to_send = response[:self.CHUNK_SIZE]
+            response = response[self.CHUNK_SIZE:]
+            yield to_send
+
+    def decompress(self):
+        """
+        разархивирование
+        """
+        decompressor = bz2.BZ2Decompressor()
+
+        # Читаем и разархивируем и отдаем строку чанками
+        while True:
+            block = self.buffer.read(self.CHUNK_SIZE)
+            if not block:
+                break
+            processed = decompressor.decompress(block)
+
+            if processed:
+                yield processed
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # -*- coding: utf-8 -*-
+from StringIO import StringIO
+from httplib import BAD_REQUEST
+
+from bottle import run, request, post, HTTPError, BaseRequest
+
+from helpers import Processor, POST_PARAM_NAME, ALLOWED_ACTIONS
+
+
+# Ограничение bottle на тело запроса
+BaseRequest.MEMFILE_MAX = 1024 * 1024
+
+
+@post('/process/<action>/')
+def process(action):
+    if request.params.keys() != [POST_PARAM_NAME] or action not in ALLOWED_ACTIONS:
+        raise HTTPError(BAD_REQUEST)
+
+    string = request.params[POST_PARAM_NAME]
+
+    compressor = Processor(StringIO(string))
+
+    # IOError означает, что Processor получил для обработки не bz2 строку
+    try:
+        for block in getattr(compressor, action)():
+            yield block
+    except IOError:
+        raise HTTPError(BAD_REQUEST)
+
+run(host='localhost', port=8080)
