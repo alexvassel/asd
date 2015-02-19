f8c4ce5f981eddc43e532f7ea45f073e067244f0
Alexander Vasilyev <av@av.users.rbc.ru>
2015-02-19 19:01:16 +0400

    refactoring + bug fixes

--- helpers.py
+++ helpers.py
@@ -15,46 +15,34 @@ class Processor(object):
     Принимает в конструкторе объект, который предоставляет методы, характерные для файла
     """
 
-    # Размер чанка для чтения
+    # Размер чанка
     CHUNK_SIZE = 32
 
-    def __init__(self, file_object):
+    def __init__(self, file_object, action):
         self.buffer = file_object
+        self.action = action
 
-    def compress(self):
+    def process(self):
         """
         архивирование
         """
-        compressor = bz2.BZ2Compressor()
+        processor = self.processor
 
-        # Читаем и файлоподобный объект чанками
+        # Читаем и отдаем содержимое запроса чанками
         while True:
             block = self.buffer.read(self.CHUNK_SIZE)
             if not block:
                 break
-            compressor.compress(block)
-
-        # Завершение процесса архивирования
-        response = compressor.flush()
-
-        # Отдаем чанками
-        while response:
-            to_send = response[:self.CHUNK_SIZE]
-            response = response[self.CHUNK_SIZE:]
-            yield to_send
-
-    def decompress(self):
-        """
-        разархивирование
-        """
-        decompressor = bz2.BZ2Decompressor()
-
-        # Читаем и разархивируем и отдаем строку чанками
-        while True:
-            block = self.buffer.read(self.CHUNK_SIZE)
-            if not block:
-                break
-            processed = decompressor.decompress(block)
+            # Архивация/разархивация
+            processed = getattr(processor, self.action)(block)
 
             if processed:
                 yield processed
+
+        # Очистить буфер в случае архивации
+        if self.action == ALLOWED_ACTIONS[0]:
+            yield processor.flush()
+
+    @property
+    def processor(self):
+        return bz2.BZ2Compressor() if self.action == ALLOWED_ACTIONS[0] else bz2.BZ2Decompressor()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from StringIO import StringIO
 from httplib import BAD_REQUEST
 
-from bottle import run, request, post, HTTPError, BaseRequest
+from bottle import run, request, post, HTTPError, BaseRequest, response
 
 from helpers import Processor, POST_PARAM_NAME, ALLOWED_ACTIONS
 
@@ -13,16 +13,19 @@ BaseRequest.MEMFILE_MAX = 1024 * 1024
 
 @post('/process/<action>/')
 def process(action):
-    if request.params.keys() != [POST_PARAM_NAME] or action not in ALLOWED_ACTIONS:
+    if POST_PARAM_NAME not in request.params or action not in ALLOWED_ACTIONS:
         raise HTTPError(BAD_REQUEST)
 
     string = request.params[POST_PARAM_NAME]
 
-    compressor = Processor(StringIO(string))
+    processor = Processor(StringIO(string), action)
+
+    response.content_type = ('text/plain' if action == ALLOWED_ACTIONS[1]
+                             else ' application/x-bzip2')
 
     # IOError означает, что Processor получил для обработки не bz2 строку
     try:
-        for block in getattr(compressor, action)():
+        for block in processor.process():
             yield block
     except IOError:
         raise HTTPError(BAD_REQUEST)
